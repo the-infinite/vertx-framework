5d22576352e5f8a29929ef451f0533d8779b9b66
the-infinite <toby.johnson.precious@gmail.com>
2026-05-16 04:21:07 +0100

    feat: improve installation script.

--- helpers/install.sh
+++ helpers/install.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
+mvn clean package
 mvn install:install-file -Dfile=./target/shared-framework.jar -DgroupId=io.github.the_infinite \
   -DartifactId=core -Dversion=1.0.0-Final -Dpackaging=jar;
