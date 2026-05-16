0bb561fcfcb23d47310dec6fbca70375c732a2cd
the-infinite <toby.johnson.precious@gmail.com>
2026-05-16 06:24:19 +0100

    fix: improve installation.

--- helpers/install.sh
+++ helpers/install.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 mvn clean package
-mvn install:install-file -Dfile=./target/shared-framework.jar -DgroupId=io.github.the_infinite \
+mvn install:install-file -Dfile=./target/framework-1.0.0-Final-jar-with-dependencies.jar -DgroupId=io.github.the_infinite \
   -DartifactId=framework -Dversion=1.0.0-Final -Dpackaging=jar;
