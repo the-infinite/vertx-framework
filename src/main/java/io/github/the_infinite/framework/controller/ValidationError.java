b581f0aa91bdd2b7965809505451ff18fdf620bb
the-infinite <toby.johnson.precious@gmail.com>
2026-05-25 15:28:37 +0100

    feat: more intuitive method of handling validation errors.

--- src/main/java/io/github/the_infinite/framework/controller/ValidationError.java
+++ src/main/java/io/github/the_infinite/framework/controller/ValidationError.java
@@ -1,10 +1,9 @@
 package io.github.the_infinite.framework.controller;
 
-import io.github.the_infinite.framework.response.ErrorResult;
-
 import java.util.HashSet;
 import java.util.Set;
 
+import io.github.the_infinite.framework.response.ErrorResult;
 import jakarta.validation.ConstraintViolation;
 
 @SuppressWarnings("unused")
@@ -16,9 +15,13 @@ public final class ValidationError extends ErrorResult {
     }).toArray(), 400);
   }
 
-  public static <T> ValidationError of(String message, ConstraintViolation<T>[] array) {
-    final var data = new HashSet<ConstraintViolation<?>>(Set.of(array));
+  public static <T> ValidationError of(String message, Set<ConstraintViolation<T>> set) {
+    final var data = new HashSet<ConstraintViolation<?>>(set);
     return new ValidationError(message, data);
+  }
+
+  public static <T> ValidationError of(String message, ConstraintViolation<T>[] array) {
+    return of(message, Set.of(array));
   }
 
   public static <T> ValidationError of(ConstraintViolation<T>[] array) {
